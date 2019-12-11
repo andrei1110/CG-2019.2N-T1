a8c7853c90a1bf1e06e240c7e00185e80d3d1832
Andrei Toledo <andrei@amplisoftware.com>
2019-12-11 20:46:17 -0300

    Alteração do modelo de pokemon, ajustes do código e animação da camera

--- src/script.js
+++ src/script.js
@@ -1,30 +1,74 @@
+const backgroundColor = 0xcccccc;
+
+var renderCalls = [];
+function render () {
+    requestAnimationFrame( render );
+    renderCalls.forEach((callback)=>{ callback(); });
+}
+render();
+
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
-var renderer = new THREE.WebGLRenderer();
+var camera = new THREE.PerspectiveCamera( 80, window.innerWidth / window.innerHeight, 0.1, 800 );
+camera.position.set(10,10,10);
+
+var renderer = new THREE.WebGLRenderer( { antialias: true } );
+renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setClearColor( backgroundColor );//0x );
 
-var loader = new THREE.ObjectLoader();
+renderer.toneMapping = THREE.LinearToneMapping;
+renderer.toneMappingExposure = Math.pow( 0.94, 5.0 );
 
-loader.load("assets/models/pikachu/pikachu-pokemon-go.json",function ( obj ) {
-		scene.add( obj );
-	},function ( xhr ) {
-		console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
-	},function ( err ) {
-		console.error( 'An error happened', err );
-	}
-);
+window.addEventListener( 'resize', function () {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}, false );
 
-camera.position.z = 5;
+document.body.appendChild( renderer.domElement);
 
-var animate = function () {
-    requestAnimationFrame( animate );
+function renderScene(){ renderer.render( scene, camera ); }
+renderCalls.push(renderScene);
 
-    // cube.rotation.x += 0.01;
-    // cube.rotation.y += 0.01;
+var controls = new THREE.OrbitControls( camera );
 
-    renderer.render( scene, camera );
-};
+controls.rotateSpeed = 0.3;
+controls.zoomSpeed = 0.5;
 
-animate();
+controls.minDistance = 1;
+controls.maxDistance = 150;
+
+controls.minPolarAngle = 0; // radians
+controls.maxPolarAngle = Math.PI /2; // radians
+
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+
+renderCalls.push(function(){
+  controls.update()
+});
+
+var light = new THREE.PointLight( 0xffffff, 40, 200 );
+light.position.set( 100, 100, 100);
+scene.add( light );
+
+var light = new THREE.PointLight( 0xffffff, 40, 200 );
+light.position.set( -100, -100, -100);
+scene.add( light );
+
+
+var loader = new THREE.GLTFLoader();
+loader.crossOrigin = true;
+loader.load( 'assets/models/charizard_final.gltf', function ( data ) {
+    var object = data.scene;
+    object.position.set(0, 0, 0);
+
+    TweenMax.from( object.position, 3, {
+        y: -2,
+        yoyo: true,
+        repeat: -1,
+        ease: 'Power2.easeInOut'
+    });
+    scene.add( object );
+});
